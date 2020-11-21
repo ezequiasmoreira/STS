d98c73122d517d141dcd642bc6ec0124532a5d38
Ezequias Moreira <ezequiasmoreira@hotmail.com>
2020-11-21 20:19:21 -0300

    Criado projeto

---
+++ src/main/java/com/ezequias/apiestudo/ApiestudoApplication.java
@@ -0,0 +1,13 @@
+package com.ezequias.apiestudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiestudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiestudoApplication.class, args);
+	}
+
+}
